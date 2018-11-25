e7b6d2695d18cbd8dcfbca7fe80f5da0e7eb2ed6
JenniferLlamas <jennifer.llamas@tectijuana.edu.mx>
2018-11-25 00:57:39 -0800

    modelo proveedores,usuarios,zona,corte,inventario,movimientos

--- easyventas/sellpoint/models.py
+++ easyventas/sellpoint/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+
+
 class Productos(models.Model):
 	nombre 				= models.CharField(max_length=200)
 	descripcion 		= models.CharField(max_length=500)
@@ -22,13 +24,32 @@ class Categoria_Productos(models.Model):
 	def __str__(self):
 		return str(self.nombre)
 
+
+class Proveedores_Clientes(models.Model):
+	nombre_comencial = models.CharField(max_length=255)
+	rfc				 = models.CharField(max_length=18)
+	direccion 		 = models.CharField(max_length=255)
+
+	def __str__(self):
+		return str(self.id)
+
+class Productos_Proveedores(models.Model):
+	producto 		 = models.ForeignKey(Productos, on_delete=models.CASCADE)
+	proveedor 		 = models.ForeignKey(Proveedores_Clientes, on_delete=models.CASCADE)
+	cantidad  		 = models.IntegerField()
+	costo_total 	 = models.FloatField()
+
+	def __str__(self):
+		return str(self.id)
+
+
 class Compras(models.Model):
 	fecha 			= models.DateField(auto_now_add=True)
 	sub_total		= models.FloatField() 
 	impuestos 		= models.FloatField()
 	total 			= models.FloatField()
 	fecha_entrega	= models.DateField()
-	""" proveedor = models.ForeignKey(Proveedores, on_delete=models.CASCADE = True)  """
+	proveedor 		= models.ForeignKey(Proveedores_Clientes,on_delete=models.CASCADE)  
 
 	def __str__(slef):
 		return str(self.id)
@@ -42,12 +63,13 @@ class  Compras_Detalle(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+
 class Ventas(models.Model):
 	fecha 	 		= models.DateTimeField(auto_now_add=True)
 	sub_total		= models.FloatField() 
 	impuestos 		= models.FloatField()
 	total 			= models.FloatField()
-	"""cliente 		= models.ForeignKey(Proveedores_Clientes, on_delete=models.CASCADE, blank = True)"""
+	cliente 		= models.ForeignKey(Proveedores_Clientes, on_delete=models.CASCADE, blank = True)
 	def __str__(self):
 		return str(self.id)
 
@@ -60,4 +82,80 @@ class Ventas_Detalle(models.Model):
 
 	def __str__(self):
 		return str(self.id)
+
+
+
+
+
+
+
+class Perfiles(models.Model):
+	perfil 			= models.CharField(max_length=100)
+
+	def __str__(self):
+		return str(self.id)
+
+
+class Usuarios(models.Model):
+	usuarios 		= models.CharField(max_length=255)
+	nombre 			= models.CharField(max_length=255)
+	password		= models.CharField(max_length=100)
+	perfil 			= models.ForeignKey(Perfiles,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.id)
+
+
+class Zona(models.Model):
+	zona 			= models.CharField(max_length=255)
+
+	def __str__(self):
+		return str(self.id)
+
+class Inventario(models.Model):
+	existencias 	= models.IntegerField()
+	producto  		= models.CharField(max_length=200)
+	zona 			= models.ForeignKey(Zona,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.id)
+
+class Almacen(models.Model):
+	almacen 		= models.CharField(max_length=255)
+	zona 			= models.ForeignKey(Zona,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.id)
+
+class Movimientos(models.Model):
+	tipo 			= models.CharField(max_length=255)
+	producto 		= models.CharField(max_length=200)
+	fecha 			= models.DateTimeField(auto_now_add=True)
+	cantidad 		= models.IntegerField()
+	zona 			= models.ForeignKey(Zona,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.id)
+
+
+
+class Corte(models.Model):
+	fecha 			= models.DateTimeField(auto_now_add=True)
+	total_vendido 	= models.IntegerField()
+	total_comprado	= models.IntegerField()
+
+	def __str__(self):
+		return str(self.id)
+
+
+class Detalle_Corte(models.Model):
+	producto 		= models.CharField(max_length=200)
+	cantidad 		= models.IntegerField()
+	total 			= models.FloatField()
+	iva 			= models.FloatField()
+	corte 			= models.ForeignKey(Corte, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.id)
+
 
