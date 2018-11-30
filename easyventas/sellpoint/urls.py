0c63fbc30e8bc4367c38a797502265a5b4421970
arraymirez <adan.ramirez17@tectijuana.edu.mx>
2018-11-30 01:01:36 -0800

    guardado  y edicion de clientes/proveedores

--- easyventas/sellpoint/urls.py
+++ easyventas/sellpoint/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     path('categorias/nuevo/', views.form_categorias_view, name="categoria_nueva_view"),
     path('productos/nuevo/', views.form_productos_view, name="producto_nuevo"),
     path('productos/buscar/', views.productos_buscar, name="producto_buscar"),
-    path('productos/editar/<int:pk>/', views.form_productos_editar.as_view(), name="productos_editar"),
+     path('productos/editar/<int:pk>/', views.form_productos_editar.as_view(), name="productos_editar"),
+
+    path('proveedores/nuevo/', views.form_proveedores_view, name="proveedor_nuevo"),
+    path('proveedores/', views.proveedores_buscar, name="proveedores_buscar"),
+    path('proveedores/editar/<int:pk>/', views.form_proveedores_editar.as_view(), name="proveedores_editar"),
+
     path('usuarios/nuevo/',views.usuario, name="usuarios_nuevo"),
     path('zona/nueva/',views.zona,name="zona_nueva"),
 
